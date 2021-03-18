b502e7136fe45e3e014855ca1153c16c1cf00b4a
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-03-18 00:04:08 -0600

    finalized

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
-
-const fs = require('fs');
+const {writeFile, copyFile} = require('./utils/generate-site');
 const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
@@ -63,7 +62,7 @@ const promptUser = () => {
 
 const promptProject = portfolioData => {
     console.log(`
-    =================
+    ================= 
     Add a New Project
     =================
     `);
@@ -144,12 +143,30 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+       
+    // We start by asking the user for their information with Inquirer prompts; this returns all of the data as an object in a Promise.
 
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log(`Page created! Check out index.html in this directory to see it!`);
-        });
+    // The promptProject() function captures the returning data from promptUser() and we recursively call promptProject() for as many projects as the user wants to add. Each project will be pushed into a projects array in the collection of portfolio information, and when we're done, the final set of data is returned to the next .then().
     
-});
+    // The finished portfolio data object is returned as portfolioData and sent into the generatePage() function, which will return the finished HTML template code into pageHTML.
+    
+    // We pass pageHTML into the newly created writeFile() function, which returns a Promise. This is why we use return here, so the Promise is returned into the next .then() method.
+    
+    // Upon a successful file creation, we take the writeFileResponse object provided by the writeFile() function's resolve() execution to log it, and then we return copyFile().
+    
+    // The Promise returned by copyFile() then lets us know if the CSS file was copied correctly, and if so, we're all done!
--- src/page-template.js
+++ src/page-template.js
@@ -70,10 +70,9 @@ module.exports = templateData => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Demo</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../dist/style 2.css">
   </head>
 
   <body>
